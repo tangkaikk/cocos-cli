f8495eabecc135e07372ab6b6ec576be3bb6693b
tangkai <1944876319@qq.com>
2026-07-24 14:42:37 +0800

    when pink dont pass android settings, onAfterInit will fetch default android setting

--- src/core/builder/platforms/google-play/src/hooks.ts
+++ src/core/builder/platforms/google-play/src/hooks.ts
@@ -3,7 +3,7 @@
 import { join } from 'path';
 import { IBuildResult, IGooglePlayInternalBuildOptions } from './type';
 import { BuilderCache, IBuilder } from '../../../@types/protected';
-import { checkAndroidAPILevels } from './utils';
+import { checkAndroidAPILevels, generateAndroidOptions } from './utils';
 import * as nativeCommonHook from '../../native-common/hooks';
 import { GlobalPaths } from '../../../../../global';
 import { getCustomIconInfo } from './custom-icon';
@@ -29,14 +29,9 @@ export async function onAfterBuild(this: IBuilder, options: IGooglePlayInternalB
 
 export async function onAfterInit(this: IBuilder, options: IGooglePlayInternalBuildOptions, result: IBuildResult, _cache: BuilderCache) {
     await nativeCommonHook.onAfterInit.call(this, options, result);
-
-    const googlePlay = options.packages['google-play'];
-    googlePlay.orientation = googlePlay.orientation || {
-        landscapeRight: true,
-        landscapeLeft: true,
-        portrait: false,
-        upsideDown: false,
-    };
+    // 没有pink传递andrdoid配置时，直接使用默认的android配置
+    const googlePlay = await generateAndroidOptions(options);
+    options.packages['google-play'] = googlePlay;
     const renderBackEnd = googlePlay.renderBackEnd;
 
     const res = await checkAndroidAPILevels(googlePlay.apiLevel, options);
